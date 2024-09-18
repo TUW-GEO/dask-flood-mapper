dccd4a515c46bc5e56452c1898dcbb694e949fa5
Martin Schobben <martin.schobben@geo.tuwien.ac.at>
2024-09-18 14:46:45 +0200

    Built site for gh-pages

--- src/dask_flood_mapper/__init__.py
+++
@@ -1,4 +0,0 @@
-from dask_flood_mapper._version import __commit__
-from dask_flood_mapper._version import __version__
-
-name = "dask_flood_mapper"
--- tests/test_meta.py
+++
@@ -1,2 +0,0 @@
-def test_that_testing_package_is_working():
-    assert True
